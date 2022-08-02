ad42fdfb44625b5aa36c1a281c8ac0c28a6a053e
Juan Manuel Pérez <jmperez.85@gmail.com>
2022-08-02 15:29:15 -0300

    Could load it!!!!

--- hf_model/load_model.js
+++ hf_model/load_model.js
@@ -2,10 +2,17 @@ const tf = require('@tensorflow/tfjs');
 const tfn = require("@tensorflow/tfjs-node");
 
 const test = async () => {
-    const handler = tfn.io.fileSystem("./convert_to_tf/prueba_layers/model.json");
-    console.log(handler);
-    //const model = await tfn.loadLayersModel("/home/jmperez/projects/tfjs-examples/hf_model/convert_to_tf/prueba_layers/model.json");
-    const model = await tfn.loadLayersModel(handler);
+    try{
+        const handler = tfn.io.fileSystem("./convert_to_tf/robertuito_js/model.json");
+
+        //const model = await tf.loadLayersModel("/home/jmperez/projects/tfjs-examples/hf_model/robertuito_js/prueba_layers/model.json");
+        const model = await tfn.loadGraphModel(handler);
+        console.log("LO CARGAMOS!")
+    }
+    catch(error){
+        console.log("ERRORRRRRRRRRRRRRRRR!!!!")
+        console.log(error);
+    }
 }
 
 (async () => {
