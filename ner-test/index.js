2b54bcad70064a953724ac170ec61e01289025d0
Juan Manuel Pérez <jmperez.85@gmail.com>
2022-08-08 17:22:07 -0300

    ANDAAAAAAAAAA

--- ner-test/index.js
+++ ner-test/index.js
@@ -7,7 +7,7 @@ let { promisify } = require('util');
 // Load tokenizer
 let tokenizer = Tokenizer.fromPretrained("finiteautomata/ner-leg");
 
-tokenizer.setPadding({ maxLength: 384 });
+tokenizer.setPadding({ maxLength: 512 });
 
 const loadModel = async () => {
     try{
@@ -23,43 +23,89 @@ const loadModel = async () => {
 }
 
 
-const encode = async (text) => {
+const tokenize = async (text) => {
     let _encode = promisify(tokenizer.encode.bind(tokenizer));
     let output = await _encode(text);
 
     return output
 }
 
-const runNERTest = async () => {
-    let output = await encode("This is a test, let's check out how it works");
-    console.log(output.getTokens());
-    console.log(output.getIds());
-    console.log(output.getAttentionMask());
+const predict = (model, tokenizedInput) => {
+    let inputIds = tf.tensor(tokenizedInput.getIds(), undefined, "int32");
+    let attentionMask = tf.tensor(tokenizedInput.getAttentionMask(), undefined, "int32");
 
-    let inputIds = tf.tensor(output.getIds(), undefined, "int32");
-    let attentionMask = tf.tensor(output.getAttentionMask(), undefined, "int32");
-
-    let inputs = {
-        "input_ids": inputIds.reshape([-1, 1]),
-        "attention_mask": attentionMask.reshape([-1, 1]),
+    let modelInput = {
+        "input_ids": inputIds.reshape([1, -1]),
+        "attention_mask": attentionMask.reshape([1, -1]),
     }
-    let model = await loadModel();
 
-    console.log("Model loaded!");
+    return model.predict(modelInput).squeeze(0);
+}
+
+let id2label = [
+    "O",
+    "B-marker",
+    "I-marker",
+    "B-reference",
+    "I-reference",
+    "B-term",
+    "I-term"
+];
+
+const decode = (prediction, tokenizedInput) => {
+    // Decode the prediction
+    // First, get the prediction for each token
+
+    let tokenPreds = prediction.argMax(1).arraySync();
+    let wordIds = tokenizedInput.getWordIds();
+    let currentWordId = null;
+
+    console.log(tokenPreds);
+    for (let i = 1; i < prediction.shape[0]; ++i) {
+        let token = tokenizedInput.getTokens()[i];
+        let pred = tokenPreds[i];
+        let wordId = wordIds[i];
+
+        if (wordId !== currentWordId) {
+            // Starts new word
+            currentWordId = wordId;
+            console.log(`${token} (${id2label[pred]})`);
+        }
+
+    }
+
+}
+
+const testString = 'a. A Person is deemed to be a holder of Registrable Securities whenever such Person owns or is deemed to own of record such Registrable Securities. If the Company receives conflicting instructions, notices or elections from two or more Persons with respect to the same Registrable Securities, the Company shall act upon the basis of instructions, notice or election received from the registered owner of such Registrable Securities.';
+
+const runNERTest = async () => {
+    let model = await loadModel();
+    console.log("Model loaded");
     console.log("Input shape");
     console.log(model.signature.inputs["input_ids:0"].tensorShape)
     console.log("Output shape");
     console.log(model.signature.outputs.logits.tensorShape);
-    let prediction = model.predict(inputs);
+    let tokenizedInput = await tokenize(testString);
+
+    console.log(tokenizedInput.getTokens());
+    console.log(tokenizedInput.getIds());
+    console.log(tokenizedInput.getAttentionMask());
+
+
+    let prediction =  predict(model, tokenizedInput);
 
     console.log(prediction);
     console.log(Object.keys(prediction));
     console.log(JSON.stringify(prediction));
 
+    decode(prediction, tokenizedInput);
+
+    let pepe = prediction.arraySync();
     const r = repl.start()
     r.context.prediction = prediction;
-    r.context.inputs = inputs;
+    r.context.tokenizedInput = tokenizedInput;
     r.context.model = model;
+    r.context.pepe = pepe;
 }
 
 runNERTest()
